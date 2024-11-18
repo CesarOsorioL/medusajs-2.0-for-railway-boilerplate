a9f9242015211d6a1fe63b48a8a5226a61a0291d
CesarOsorioL <168827212+Support-ImmiHub@users.noreply.github.com>
2024-11-18 11:58:11 +1100

    change
    change

--- storefront/src/modules/products/components/product-preview/index.tsx
+++ storefront/src/modules/products/components/product-preview/index.tsx
@@ -47,17 +47,15 @@ export default async function ProductPreview({
           </div>
         </div>
         <div className="flex items-center space-x-2">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 text-[#A26501]">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-  </svg>
-  
-  <span className="text-ui-fg-subtle text-[#A26501]">
-    Medellin
-  </span>
-</div>
-
-
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 text-[#A26501]">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+          </svg>
+          
+          <span className="text-ui-fg-subtle text-[rgba(162,101,1,0.8)]">
+            Medellin
+          </span>
+        </div>
       </div>
     </LocalizedClientLink>
   )
