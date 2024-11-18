4b30e741cb6f0421b13e9e8fc94841c303243cb3
CesarOsorioL <168827212+Support-ImmiHub@users.noreply.github.com>
2024-11-18 11:12:16 +1100

    fixing content
    fixing content

--- storefront/src/modules/products/components/product-preview/index.tsx
+++ storefront/src/modules/products/components/product-preview/index.tsx
@@ -46,16 +46,17 @@ export default async function ProductPreview({
             {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
           </div>
         </div>
-        <div className="flex">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+        <div className="flex items-center space-x-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 text-[#A26501]">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
           </svg>
+          
+          <span className="text-ui-fg-subtle text-[rgba(162,101,1,0.8)]">
+            Medellin
+          </span>
+        </div>
 
-          <Text className="text-ui-fg-subtle text-[rgba(162,101,1,0.8)]">
-              Medellin
-            </Text>
-          </div>
       </div>
     </LocalizedClientLink>
   )
