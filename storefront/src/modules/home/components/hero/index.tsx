3ef72aa405d24f13338539b594c3088895ad4671
CesarOsorioL <168827212+Support-ImmiHub@users.noreply.github.com>
2025-04-09 15:47:15 +1000

    image mobile banner
    image mobile banner

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,21 +1,42 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 import Image from "next/image"
+import { useEffect, useState } from "react"
 
 
 const Hero = () => {
+  const [imageSrc, setImageSrc] = useState("mobile-banner.png")
+
+  useEffect(() => {
+    const updateImage = () => {
+      const width = window.innerWidth
+      if (width < 768) {
+        setImageSrc("/mobile-banner.png")
+      } else if (width < 1200) {
+        setImageSrc("/images/banner/banner-tablet.jpg")
+      } else {
+        setImageSrc("/banner.png")
+      }
+    }
+
+    updateImage()
+    window.addEventListener("resize", updateImage)
+    return () => window.removeEventListener("resize", updateImage)
+  }, [])
+
+
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       {/* <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"> */}
         <Image
-            src="/banner.png" // Asegúrate de que la imagen esté en la carpeta /public
+            src={imageSrc} // Asegúrate de que la imagen esté en la carpeta /public
             alt="Banner Image"
             fill
             // width={300}
             // height={200}
-            sizes="(max-width: 768px) 100vw,
-         (max-width: 1200px) 100vw,
-         100vw"
+        //     sizes="(max-width: 768px) 100vw,
+        //  (max-width: 1200px) 100vw,
+        //  100vw"
            className="object-cover object-center"
            priority
           />
